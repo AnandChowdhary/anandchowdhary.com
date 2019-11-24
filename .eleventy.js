fa555797a8a70c2fcd5a099d4cdfc336b7eff8e9
Anand Chowdhary <mail@anandchowdhary.com>
2019-11-23 19:15:53 -0800

    :sparkles: Add post excerpt

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addNunjucksFilter("encodeURIComponent", value => encodeURIComponent(value));
+  eleventyConfig.addShortcode("excerpt", post => extractExcerpt(post));
   eleventyConfig.addNunjucksFilter("place", value => {
     switch (value) {
       case "san-francisco-bay-area":
@@ -34,3 +35,27 @@ module.exports = (eleventyConfig) => {
     }
   };
 };
+
+/**
+ * Extracts the excerpt from a document.
+ *
+ * @param {*} doc A real big object full of all sorts of information about a document.
+ * @returns {String} the excerpt.
+ * @source https://github.com/11ty/eleventy/issues/410#issuecomment-462821193
+ */
+function extractExcerpt(doc) {
+  if (!doc.hasOwnProperty("templateContent")) {
+    console.warn("❌ Failed to extract excerpt: Document has no property `templateContent`.");
+    return;
+  }
+  const excerptSeparator = "<!--more-->";
+  const content = doc.templateContent;
+  if (content.includes(excerptSeparator)) {
+    return content.substring(0, content.indexOf(excerptSeparator)).trim();
+  }
+  const pCloseTag = "</p>";
+  if (content.includes(pCloseTag)) {
+    return content.substring(0, content.indexOf(pCloseTag) + pCloseTag.length);
+  }
+  return content;
+}
