38a07615d4bd3869c08225d7901a5f0d6ad32b54
Anand Chowdhary <mail@anandchowdhary.com>
2020-01-27 20:59:57 +0530

    :sparkles: Add pocket casts history

--- helpers/life-data.js
+++ helpers/life-data.js
@@ -16,8 +16,16 @@ const spotify = async () => {
   await writeJson(join(CONTENT_DATA_DIR, "shortTermArtists.json"), shortTermArtists);
 }
 
+const pocketCasts = async () => {
+  const pocketCastsPodcasts = safeLoad(await readFile(join(LIFE_DATA_DIR, "podcasts.yml"), "utf8"));
+  await writeJson(join(CONTENT_DATA_DIR, "pocketCastsPodcasts.json"), pocketCastsPodcasts);
+  const pocketCastsHistory = safeLoad(await readFile(join(LIFE_DATA_DIR, "podcast-history.yml"), "utf8"));
+  await writeJson(join(CONTENT_DATA_DIR, "pocketCastsHistory.json"), pocketCastsHistory);
+}
+
 const lifeDataUtilities = async () => {
   await spotify();
+  await pocketCasts();
 }
 
 lifeDataUtilities();
