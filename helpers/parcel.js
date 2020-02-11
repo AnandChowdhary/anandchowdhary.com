539e3610600b3b11b2c88fb52c4698f0cb5f2407
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-11 18:11:54 +0100

    :sparkles: Add parcel CSS script

--- helpers/parcel.js
+++ helpers/parcel.js
@@ -39,6 +39,41 @@ const copyParcelJs = async () => {
   );
 };
 
+const copyParcelCss = async () => {
+  const files = await readdir(join(__dirname, "..", "dist"));
+  const cssFile = files.filter(f => f.endsWith(".css"))[0];
+  await ensureDir(join(__dirname, "..", "public", "assets", "styles"));
+  await copyFile(
+    join(__dirname, "..", "dist", cssFile),
+    join(__dirname, "..", "public", "assets", "styles", cssFile)
+  );
+  try {
+    await copyFile(
+      join(__dirname, "..", "dist", `${cssFile}.map`),
+      join(__dirname, "..", "public", "assets", "styles", `${cssFile}.map`)
+    );
+  } catch (error) {}
+  const integrityMap = await fromStream(
+    createReadStream(
+      join(__dirname, "..", "public", "assets", "styles", cssFile)
+    ),
+    {
+      algorithms: ["sha384"]
+    }
+  );
+  await writeJson(
+    join(__dirname, "..", "content", "_data", "cssFilePath.json"),
+    {
+      name: cssFile,
+      integrity: integrityMap.toString()
+    }
+  );
+};
+
 copyParcelJs()
   .then(() => console.log("Copied parcel JS"))
+  .catch(error => console.log(error));
+
+copyParcelCss()
+  .then(() => console.log("Copied parcel CSS"))
   .catch(error => console.log(error));
