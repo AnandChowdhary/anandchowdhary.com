83f84a1236f4024e0013e78cf6f58a936b21bf47
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-11 18:05:56 +0100

    :heavy_minus_sign: Remove gulp css dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,7 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
-var concat = require("gulp-concat");
-var rename = require("gulp-rename");
-var cleanCSS = require("gulp-clean-css");
 var del = require("del");
 
 var paths = {
-  styles: {
-    src: "styles/**/*.scss",
-    dest: "public/assets/styles/"
-  },
   static: {
     src: "static/**/*.*",
     dest: "public/"
@@ -20,35 +12,18 @@ function clean() {
   return del(["public/assets"]);
 }
 
-function styles() {
-  return gulp
-    .src(paths.styles.src.replace("**/*", "app"))
-    .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(concat("main.min.css"))
-    .pipe(
-      rename({
-        basename: "main",
-        suffix: ".min"
-      })
-    )
-    .pipe(gulp.dest(paths.styles.dest));
-}
-
 function static() {
   return gulp.src(paths.static.src).pipe(gulp.dest(paths.static.dest));
 }
 
 function watch() {
-  gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.static.src, static);
 }
 
-var build = gulp.series(clean, gulp.parallel(styles, static));
+var build = gulp.series(clean, gulp.parallel(static));
 
 exports.clean = clean;
 exports.static = static;
-exports.styles = styles;
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
