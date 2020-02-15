32f6101237c51e6ef796a60eb359cdacf7303f05
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-15 18:12:24 +0100

    :rotating_light: Lint sitemap.xml

--- helpers/sitemap.js
+++ helpers/sitemap.js
@@ -33,20 +33,15 @@ const generateSitemap = async () => {
     markdownLibrary.render(newHtml)
   );
   await writeFile(join(__dirname, "..", "public", "sitemap/index.html"), HTML);
-  const XML = `
-  <?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  const XML = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   ${files
     .map(
-      i => `
-    <url>
-      <loc>https://anandchowdhary.com${i}</loc>
-    </url>
-  `
+      i => `<url>
+  <loc>https://anandchowdhary.com${i}</loc>
+</url>`
     )
-    .join("")}
-  </urlset>
-  `;
+    .join("\n")}
+  </urlset>`;
   await writeFile(join(__dirname, "..", "public", "sitemap.xml"), XML);
 };
 
