5689e9b68035c13f7c55ed1e64fbb58b35918ddc
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-16 13:28:48 +0100

    :bug: Fix cities path

--- helpers/cities.js
+++ helpers/cities.js
@@ -1,34 +1,32 @@
 const { readJsonSync } = require("fs-extra");
 const { join } = require("path");
 
-let cityData = [];
-
-try {
-  cityData = readJsonSync(join(__dirname, "..", "cache", "cities.json"));
-} catch (error) {}
+let cityData = readJsonSync(
+  join(__dirname, "..", "content", "_data", "cities.json")
+);
 
 const getCityEmoji = city => {
   const thisCity = cityData.filter(i => i.slug === city);
-  if (thisCity.length) return thisCity.emoji;
+  if (thisCity.length) return thisCity[0].flag;
   return "";
 };
 
 const getCityName = city => {
   const thisCity = cityData.filter(i => i.slug === city);
-  if (thisCity.length) return thisCity.title;
+  if (thisCity.length) return thisCity[0].title;
   return "";
 };
 
 const getCityCountry = city => {
   const thisCity = cityData.filter(i => i.slug === city);
   if (thisCity.length)
-    return `<div>${thisCity.emoji} ${thisCity.country}</div>`;
+    return `<div>${thisCity[0].flag} ${thisCity[0].country}</div>`;
   return "";
 };
 
 const getCityEmojiTitle = city => {
   const thisCity = cityData.filter(i => i.slug === city);
-  if (thisCity.length) return `${thisCity.emoji} ${thisCity.country}`;
+  if (thisCity.length) return `${thisCity[0].flag} ${thisCity[0].title}`;
   return "";
 };
 
