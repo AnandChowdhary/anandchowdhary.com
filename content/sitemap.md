9c1da9c7067734d868d3282e954496f25a437014
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-25 12:01:04 +0100

    :bug: Remove "REPLACE_SITEMAP" from SEO

--- content/sitemap.md
+++ content/sitemap.md
@@ -3,6 +3,4 @@ title: Sitemap
 layout: page.njk
 ---
 
-This is a list of all pages on this website:
-
-REPLACE_SITEMAP
+This is a list of all pages on this website. If you're a human or search engine, this might help you find what you're looking for:
--- helpers/sitemap.js
+++ helpers/sitemap.js
@@ -29,8 +29,8 @@ const generateSitemap = async () => {
     await readFile(join(__dirname, "..", "public", "sitemap/index.html"))
   ).toString();
   const HTML = currentContents.replace(
-    "<p>REPLACE_SITEMAP</p>",
-    markdownLibrary.render(newHtml)
+    "</p>",
+    "</p>" + markdownLibrary.render(newHtml)
   );
   await writeFile(join(__dirname, "..", "public", "sitemap/index.html"), HTML);
   const XML = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
