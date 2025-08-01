5b741e242c56297b161cac63097edd655534b2c9
Anand Chowdhary <mail@anandchowdhary.com>
2025-08-01 18:29:49 +0200

    :fire: Delete all files and start afresh

--- routes/_404.tsx
+++
@@ -1,5 +0,0 @@
-import { UnknownPageProps } from "$fresh/server.ts";
-
-export default function NotFoundPage({ url }: UnknownPageProps) {
-  return <p class="text-center">404 Not Found: {url.pathname}</p>;
-}
