ec35b816cc0ba74d8684b3ac839a089dea84d546
Anand Chowdhary <mail@anandchowdhary.com>
2025-11-10 10:44:27 +0100

    :bug: Enhance Container component with Suspense and parameter handling
    Refactor the Container component to include a Suspense wrapper for improved loading states. Introduce ContainerContentWithParams to manage search parameters, specifically for screenshot mode, enhancing the component's functionality and user experience. This change promotes better handling of asynchronous content rendering.

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -2,10 +2,37 @@
 
 import { useSearchParams } from "next/navigation";
 import type { ReactNode } from "react";
+import { Suspense } from "react";
 
 export function Container({ children }: { children: ReactNode }) {
+  return (
+    <Suspense
+      fallback={
+        <ContainerContent isScreenshotMode={false}>{children}</ContainerContent>
+      }
+    >
+      <ContainerContentWithParams>{children}</ContainerContentWithParams>
+    </Suspense>
+  );
+}
+
+function ContainerContentWithParams({ children }: { children: ReactNode }) {
   const searchParams = useSearchParams();
   const isScreenshotMode = searchParams.get("screenshot") === "true";
+  return (
+    <ContainerContent isScreenshotMode={isScreenshotMode}>
+      {children}
+    </ContainerContent>
+  );
+}
+
+function ContainerContent({
+  children,
+  isScreenshotMode,
+}: {
+  children: ReactNode;
+  isScreenshotMode: boolean;
+}) {
   return (
     <div
       className={`font-sans min-h-screen pb-20 gap-16 ${
